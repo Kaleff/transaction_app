093df9c0b44a76c5e47677287e0c014bca67e297
Marks Kalijevs <markskalijevs@gmail.com>
2024-02-16 03:25:20 +0200

    Make basic controllers and models for 4 tables

--- laravel-backend/database/seeders/DatabaseSeeder.php
+++ laravel-backend/database/seeders/DatabaseSeeder.php
@@ -3,20 +3,23 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Create dummy admin entry.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::where('email', 'admin@bank.com')->firstOr(function () {
+            User::create([
+                'name' => 'admin',
+                'email' => 'admin@bank.com',
+                'password' => Hash::make('password'),
+            ]);
+        });
     }
 }
---
+++ laravel-backend/database/seeders/ExchangeRateSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ExchangeRate;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ExchangeRateSeeder extends Seeder
+{
+    /**
+     * Populate the currency table with base currency info if not done before, and update rates based on 3-rd party resource
+     */
+    public function run(): void
+    {
+        ExchangeRate::where('currency', 'JPY')->firstOr(function () {
+            ExchangeRate::createMany(config('currency.rates'));
+        });
+    }
+}
