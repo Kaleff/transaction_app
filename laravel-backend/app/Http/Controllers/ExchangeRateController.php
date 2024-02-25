f51f6b5f1a52e0cd94f53010890859a0e19a4ebf
Marks Kalijevs <markskalijevs@gmail.com>
2024-02-25 17:00:18 +0200

    Adapt exchange rate store for api interaction

--- laravel-backend/app/Http/Controllers/ExchangeRateController.php
+++ laravel-backend/app/Http/Controllers/ExchangeRateController.php
@@ -13,19 +13,18 @@ class ExchangeRateController extends Controller
     public function store(Request $request)
     {
         // Check if base currency is Euro and request was successful
-        /*if($request->base != "EUR" || !$request->success) {
-            return [];
-        } */
-        $query = [];
-        foreach(config('currency.api_sample') as $currency => $rate) {
-            $query[] = [
-                'currency' => $currency,
-                'exchange_rate' => $rate
-            ];
+        if($request->base != "EUR" || !$request->success || count($request->rates) == 0) {
+            return response()->json([
+                'success' => false,
+                'errors' => ['Invalid request'],
+            ], 406);
         }
-        ExchangeRate::upsert($query, [
-            ['currency', 'name'], ['exchange_rate']
+        foreach($request->rates as $currency => $rate) {
+            ExchangeRate::where('currency', $currency)->update(['exchange_rate' => $rate]);
+        }
+        return response()->json([
+            'success' => true,
+            'message' => 'Exchange rates where updated successfully'
         ]);
-        return response()->json($query);
     }
 }
